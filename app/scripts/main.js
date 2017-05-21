c58dc657f7716a2d9087a724634d52bd5ad4fa2d
rudycrespo <rudy.crespo20@gmail.com>
2017-05-21 13:22:47 -0400

    setting datetime attribute

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,10 +5,13 @@
 		const $clock = document.getElementById(clockEl);
 		const activeClass = 'active';
 
-		const setTimePeriod = () => { $clock.querySelector( `#${moment().format('a')}`).classList.add(activeClass ) };
-		const setDay = () => { $clock.querySelector( `.clock__day[data-day="${moment().day()}"]` ).classList.add(activeClass ) };
+		const setTimePeriod = () => { $clock.querySelector( `#${moment().format('a')}`).classList.add(activeClass) };
+		const setDay = () => { $clock.querySelector( `.clock__day[data-day="${moment().day()}"]` ).classList.add(activeClass) };
+
+		let alarmTime = '';
 
 		function setTime() {
+			const $timeDigits = $clock.querySelector('#time-digits');
 			const $hours = $clock.querySelector('#hours');
 			const $minutes = $clock.querySelector('#minutes');
 			const $seconds = $clock.querySelector('#seconds');
@@ -17,6 +20,8 @@
 			$minutes.innerHTML = moment().format('mm');
 			$seconds.innerHTML = moment().format('ss');
 
+			$timeDigits.setAttribute('datetime', moment().format('hh:mm:ss'));
+
 			setTimePeriod();
 			setDay();
 		}
@@ -24,6 +29,7 @@
 		return {
 			init: () => {
 				setInterval(setTime, 1000);
+				console.log()
 			}
 		}
 	};
